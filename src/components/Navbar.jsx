74bf028eb660bee782a1e3eecfbd363d233eb3b7
deepeshyadavrd <raoshsbh6@gmail.com>
2024-08-31 16:31:41 +0530

    css updated for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className='p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200'>
-        <h1>Navbar</h1>
+      <div className='flex justify-betweenitems-center space-x-5 w-screen'>
+        <Link to="/">
+          <p className='text-2xl bg-blue-500 font-bold text-white'>
+            Goggle
+          </p>
+        </Link>
+      </div>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router,Link } from "react-router-dom";
+
 import App from "./App";
 import "./index.css";
 
 const root = createRoot(document.getElementById('root'));  
-root.render(<App  />);
+root.render(
+    <Router>
+        <App  />
+    </Router>
+);
