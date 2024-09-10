5e226f02cb74895ed20c07ccb72e9fde0913cdca
deepeshyadavrd <raoshsbh6@gmail.com>
2024-09-10 15:06:54 +0530

    .

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,7 +2,11 @@ import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 
+import { useResultContext } from '../Contexts/ResultContextProvider';
+
 const Results = () => {
+
+  const { results, isLoading, getResults, searchTerm } = useResultContext();
   return (
     <div>Results</div>
   )
