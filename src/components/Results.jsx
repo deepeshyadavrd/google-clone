783cc234411555eab348e8bfcf0107782960d435
deepeshyadavrd <raoshsbh6@gmail.com>
2024-11-07 17:06:37 +0530

    ,.

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -11,18 +11,27 @@ const Results = () => {
   const location = useLocation();
 
     useEffect(() => {
-      getResults('/search/q=Javascript Mastery&num=40')
+      getResults('?query=Javascript Mastery&limit=40')
     }, []);
   if (isLoading) return <Loading />
-  // console.log(location.pathname);
   
   switch (location.pathname) {
     case '/search':
       return (
         <div className='flex flex-wrap justify-between space-y-6 sm:px-56'>
-          {/* {results?.results?.map( ({ link, title })=>(
+          {results?.results?.map( ({ url, title }, index)=>(
+            <div key={index} className='md:w-2/5 w-full'>
+              <a href={url} target="_blank" rel="noreferrer">
+                <p className='text-sm'>
+                  {url.length > 30 ? url.substring(0, 30) : url}
+                </p>
+                <p className='text-lg hover:underline dark:text-blue-300 text-blue-700'>
+                  {title}
 
-          ))} */}
+                </p>
+              </a>
+            </div>
+          ))}
         </div>
       )
     case '/images':
