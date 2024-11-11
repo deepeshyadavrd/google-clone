4fccc325e5f4790f2361e6345815ec52bff19de8
deepeshyadavrd <raoshsbh6@gmail.com>
2024-11-11 11:05:48 +0530

    new section updated

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -58,17 +58,17 @@ const Results = () => {
       )
     case '/news':
       return (
-        <div className='flex flex-wrap justify-between space-y-6 sm:px-56'>
-          {results?.news_results?.map( ({ url, title }, index)=>(
+        <div className='flex flex-wrap justify-between space-y-6 sm:px-56 items-center'>
+          {results?.news_results?.map( ({ url, title, source_name }, index)=>(
             <div key={index} className='md:w-2/5 w-full'>
-              <a href={url} target="_blank" rel="noreferrer">
-                <p className='text-sm'>
-                  {url.length > 30 ? url.substring(0, 30) : url}
-                </p>
-                <p className='text-lg hover:underline dark:text-blue-300 text-blue-700'>
+              <a href={url} target="_blank" rel="noreferrer"  className='hover:underline'>
+              
+                <p className='text-lg dark:text-blue-300 text-blue-700'>
                   {title}
-
                 </p>
+                <div className='flex gap-4'>
+                  {source_name}
+                </div>
               </a>
             </div>
           ))}
